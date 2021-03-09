7e562c5622325ef7ba5e04a4b96445395ac05361
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-03-09 23:27:47 +0530

    Add One Row To Tree

---
+++ LeetCodeProblems/src/add_one_row_to_tree/Runner.java
@@ -0,0 +1,18 @@
+package add_one_row_to_tree;
+
+import main.TreeNode;
+
+public class Runner {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right= new TreeNode(1);
+
+        root.right  = new TreeNode(6);
+        root.right.left  = new TreeNode(5);
+
+        Solution s = new Solution();
+        s.addOneRow(root, 1, 4);
+    }
+}
