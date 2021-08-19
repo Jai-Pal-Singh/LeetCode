0f87d476033b6f94f7fc8ac662e75ea32a469564
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-08-19 09:21:17 +0530

    best time to buy and sell stock II

---
+++ LeetCodeProblems/src/best_time_to_buy_and_sell_stock_II/Runner.java
@@ -0,0 +1,8 @@
+package best_time_to_buy_and_sell_stock_II;
+
+public class Runner {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.maxProfit(new int [] {7,1,5,3,6,4}));
+    }
+}
