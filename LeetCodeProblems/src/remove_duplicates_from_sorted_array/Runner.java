9f972f4ee85bfe65525aff657426fc1f32bdfd5f
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-08-19 08:50:30 +0530

    remove duplicates from sorted array

---
+++ LeetCodeProblems/src/remove_duplicates_from_sorted_array/Runner.java
@@ -0,0 +1,8 @@
+package remove_duplicates_from_sorted_array;
+
+public class Runner {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.removeDuplicates(new int [] {0,0,1,1,1,2,2,3,3,4}));
+    }
+}
