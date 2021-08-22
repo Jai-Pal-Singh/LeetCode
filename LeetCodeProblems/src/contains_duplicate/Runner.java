684681390c0621f6fe001bcff82746bdd6eefe35
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-08-22 20:35:27 +0530

    contains duplicate

---
+++ LeetCodeProblems/src/contains_duplicate/Runner.java
@@ -0,0 +1,8 @@
+package contains_duplicate;
+
+public class Runner {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.containsDuplicate(new int [] {1,1,1,3,3,4,3,2,4,2}));
+    }
+}
