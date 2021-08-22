6c14354e7eb888a1b65c61e1274e9021777c28fa
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-08-22 19:41:01 +0530

    rotate array

---
+++ LeetCodeProblems/src/helper/ArrayFunctions.java
@@ -0,0 +1,10 @@
+package helper;
+
+import java.util.Arrays;
+
+public class ArrayFunctions {
+    
+    public static void print(int[] nums){
+        System.out.println(Arrays.toString(nums));
+    }
+}
---
+++ LeetCodeProblems/src/rotate_array/Runner.java
@@ -0,0 +1,8 @@
+package rotate_array;
+
+public class Runner {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.rotate(new int [] {1,2}, 3);
+    }
+}
