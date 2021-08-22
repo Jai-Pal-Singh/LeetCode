8b54e280011ef41e9015cc875215f8a21e5e8506
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-08-22 21:53:56 +0530

    intersection of two arrays II

---
+++ LeetCodeProblems/src/intersection_of_two_arrays_II/Runner.java
@@ -0,0 +1,8 @@
+package intersection_of_two_arrays_II;
+
+public class Runner {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        helper.ArrayFunctions.print(solution.intersect(new int[]{4,9,5}, new int[]{9,4,9,8,4}));
+    }
+}
