c4cee5e258faad0efd99775b6699780d7e72328d
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-08-22 21:23:23 +0530

    single number

---
+++ LeetCodeProblems/src/single_number/Runner.java
@@ -0,0 +1,8 @@
+package single_number;
+
+public class Runner {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.singleNumber(new int[] {4,1,2,1,2}));
+    }
+}
