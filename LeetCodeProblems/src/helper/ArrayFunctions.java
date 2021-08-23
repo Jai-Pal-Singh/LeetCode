ecbd6d5cdd6dcce158da864c8edcf7e19b44ee2a
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-08-23 14:52:04 +0530

    rotate image

--- LeetCodeProblems/src/helper/ArrayFunctions.java
+++ LeetCodeProblems/src/helper/ArrayFunctions.java
@@ -7,4 +7,10 @@ public class ArrayFunctions {
     public static void print(int[] nums){
         System.out.println(Arrays.toString(nums));
     }
+
+    public static void print(int[][] nums){
+        for (int[] num : nums) {
+            System.out.println(Arrays.toString(num));
+        }       
+    }
 }
---
+++ LeetCodeProblems/src/rotate_image/Runner.java
@@ -0,0 +1,9 @@
+package rotate_image;
+
+public class Runner {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        Solution sol = new Solution();
+        sol.rotate(matrix);
+    }
+}
