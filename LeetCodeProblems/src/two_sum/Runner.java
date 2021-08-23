8d550de9906e90b59e018569c122c2c342ecc936
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-08-23 13:52:29 +0530

    two sum -> method 2

--- LeetCodeProblems/src/two_sum/Runner.java
+++ LeetCodeProblems/src/two_sum/Runner.java
@@ -2,7 +2,10 @@ package two_sum;
 
 public class Runner {
     public static void main(String[] args) {
-        Solution sol = new Solution();
-        System.out.println(sol.twoSum(new int[]{2,7,11,15}, 9));
+        // Solution sol = new Solution();
+        // System.out.println(sol.twoSum(new int[]{2,7,11,15}, 9));
+
+        Solution2 sol2 = new Solution2();
+        helper.ArrayFunctions.print(sol2.twoSum(new int[]{2,7,11,15}, 9));
     }
 }
