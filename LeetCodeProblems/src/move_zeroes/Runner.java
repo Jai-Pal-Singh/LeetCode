c9fb8f877f4ec55a7d11beb54b4aae687dd6618f
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-08-23 12:14:57 +0530

    move zeroes

---
+++ LeetCodeProblems/src/move_zeroes/Runner.java
@@ -0,0 +1,12 @@
+package move_zeroes;
+
+/**
+ * Runner
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.moveZeroes(new int [] {0,1,0,3,12});
+    }
+}
