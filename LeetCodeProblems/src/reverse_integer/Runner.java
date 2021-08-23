8d9a6247283a403eab324bd5235da23dad68612a
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-08-23 19:45:50 +0530

    reverse integer

---
+++ LeetCodeProblems/src/reverse_integer/Runner.java
@@ -0,0 +1,8 @@
+package reverse_integer;
+
+public class Runner {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.reverse(Integer.MIN_VALUE - 1));
+    }
+}
