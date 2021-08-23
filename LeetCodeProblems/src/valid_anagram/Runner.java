da35494e3758ece707eb60377a6cdc259f13ef6b
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-08-23 20:52:17 +0530

    Valid Anagram

---
+++ LeetCodeProblems/src/valid_anagram/Runner.java
@@ -0,0 +1,8 @@
+package valid_anagram;
+
+public class Runner {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.isAnagram("anagram", "nagaram"));
+    }
+}
---
+++ LeetCodeProblems/src/valid_anagram/Solution.java
@@ -0,0 +1,38 @@
+package valid_anagram;
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
+ 
+
+// Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+        for (int i : counter) {
+            if(i != 0) return false;
+        }
+        return true;
+    }
+}
