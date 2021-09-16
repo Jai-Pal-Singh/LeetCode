fe9813853321bb7cfd14f4106a343098c85db660
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-09-16 08:55:48 +0530

    longest turbulent subarray

---
+++ LeetCodeProblems/src/longest_turbulent_subarray/Runner.java
@@ -0,0 +1,8 @@
+package longest_turbulent_subarray;
+
+public class Runner {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.maxTurbulenceSize(new int [] {9,4,2,10,7,8,8,1,9}));
+    }
+}
