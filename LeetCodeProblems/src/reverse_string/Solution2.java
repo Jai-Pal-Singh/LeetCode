574f2a2d84976762d9f0d86c4e7aa11a951759d1
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-09-29 09:27:23 +0530

    reverse string - recursive solution

---
+++ LeetCodeProblems/src/reverse_string/Solution2.java
@@ -0,0 +1,19 @@
+package reverse_string;
+//using Recursion
+public class Solution2 {
+    public void reverseString(char[] s) {
+        int start = 0, end = s.length-1;
+        reverseString(s, start, end);
+    }
+    public void reverseString(char[] s, int start, int end) {
+        char temp;
+        if(start < end) {
+            temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++; end--;
+            reverseString(s, start, end);
+        }
+        else return;
+    }
+}
