d40c51f4a8c3c88c9ebdc38a6b43360ac3899e69
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-10-02 17:23:03 +0530

    pow(x,n)

---
+++ LeetCodeProblems/src/pow_x_n/Runner.java
@@ -0,0 +1,8 @@
+package pow_x_n;
+
+public class Runner {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.myPow(2.000000, 10));
+    }
+}
