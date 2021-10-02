cb5f817c03d04322cc5c00cd939277b426fac3f3
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-10-02 12:19:14 +0530

    fibonacci number

---
+++ LeetCodeProblems/src/fibonacci_number/Runner.java
@@ -0,0 +1,8 @@
+package fibonacci_number;
+
+public class Runner {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.fib(6));
+    }
+}
