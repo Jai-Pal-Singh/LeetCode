81ebc2571c47009bd35cdebca68272ea9fcb3a2e
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-11-25 08:37:54 +0530

    House Robber II

---
+++ LeetCodeProblems/src/house_robber_ii/Runner.java
@@ -0,0 +1,10 @@
+package house_robber_ii;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        int[] nums = {2, 1, 1, 2};
+        BasicFunctions.print(new Solution().rob(nums));
+    }        
+}
