f706a7e9f208843405c95c282c9a1df90bc30f21
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-11-29 07:34:08 +0530

    bug fix

--- Learn/src/test/Question2.java
+++ Learn/src/test/Question2.java
@@ -1,21 +1,26 @@
 package test;
 
-public class Question2 {
-    public static void main(String[] args) {
-        Question2 q2 = new Question2();
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-        System.out.println(q2.path(new int[][]{{2,5,10}, {4,8,9}, {1,3,8}}, 0, 0));
+public class Question2 {
+
+    private static Logger log = Logger.getLogger(Question2.class.getName());
+    public static void main(String[] args) {
+
+        log.log(Level.INFO, "Output : {0}", new Question2().path(new int[][]{{2,5,10}, {4,8,9}, {1,3,8}}, 0, 0));
     }
 
     private int path(int[][] matrix, int i, int j) {
         if(i < matrix.length && j < matrix[0].length){
             int bottom = trailingZeros(path(matrix, i+1, j));
             int next = trailingZeros(path(matrix, i, j+1));
+            return Math.min(bottom, next);
         }
         return 0;
     }
 
     private int trailingZeros(int path) {
-        return 0;
+        return path;
     }
 }
