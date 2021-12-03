e8528b4df285366729abdaa89eb74da370d8ecde
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-12-03 12:25:33 +0530

    Linked List Cycle

---
+++ LeetCodeProblems/src/linked_list_cycle/Runner.java
@@ -0,0 +1,15 @@
+package linked_list_cycle;
+
+import helper.ListNode;
+
+public class Runner {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = head.next.next;
+        helper.BasicFunctions.print(new Solution().hasCycle(head));
+    }
+}
