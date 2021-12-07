b719999c5c6da80713b66c529ae325edcc6f2ade
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-12-07 07:55:09 +0530

    Longest Palindromic Substring

---
+++ LeetCodeProblems/src/longest_palindromic_substring/Runner.java
@@ -0,0 +1,8 @@
+package longest_palindromic_substring;
+
+public class Runner {
+    public static void main(String[] args) {
+        String s = "babad";
+        helper.BasicFunctions.print(new Solution().longestPalindrome(s));
+    }
+}
