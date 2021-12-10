acbe29f380567c52dc42af7ecd4eebf60a4556f5
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-12-10 22:23:14 +0530

    Best Sightseeing Pair

---
+++ LeetCodeProblems/src/best_sightseeing_pair/Runner.java
@@ -0,0 +1,7 @@
+package best_sightseeing_pair;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().maxScoreSightseeingPair(new int[] {8, 1, 5, 2, 6}));
+    }
+}
