d8f9318e572fee783cdf0d9357ed845a9e4baf55
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-12-10 20:36:45 +0530

    Wiggle Subsequence

---
+++ LeetCodeProblems/src/wiggle_subsequence/Runner.java
@@ -0,0 +1,8 @@
+package wiggle_subsequence;
+
+public class Runner {
+    public static void main(String[] args) {
+        int[] nums = {1, 7, 4, 9, 2, 5};
+        helper.BasicFunctions.print(new Solution().wiggleMaxLength(nums));
+    }
+}
