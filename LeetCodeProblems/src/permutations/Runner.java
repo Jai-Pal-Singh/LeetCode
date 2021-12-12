a122ec0e40a9b840f4c62f9edf5ec4b5ca3c98d0
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-12-12 16:22:34 +0530

    Permutations

---
+++ LeetCodeProblems/src/permutations/Runner.java
@@ -0,0 +1,7 @@
+package permutations;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.ArrayFunctions.printList(new Solution().permute(new int[] { 1, 2, 3 }));
+    }
+}
