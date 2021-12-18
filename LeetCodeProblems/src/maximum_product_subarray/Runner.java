85440f615e8f492c0119f4c376a908db1a1c586c
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-12-18 21:50:01 +0530

    Maximum Product Subarray

---
+++ LeetCodeProblems/src/maximum_product_subarray/Runner.java
@@ -0,0 +1,9 @@
+package maximum_product_subarray;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().maxProduct(new int[] {1,-2,-3,4}));
+    }
+}
