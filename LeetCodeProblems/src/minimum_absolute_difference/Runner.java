acf6ccea853e457d8481f7cc84489fa042261fa4
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-12-20 10:12:07 +0530

    Minimum Absolute Difference

---
+++ LeetCodeProblems/src/minimum_absolute_difference/Runner.java
@@ -0,0 +1,9 @@
+package minimum_absolute_difference;
+
+import helper.ArrayFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        ArrayFunctions.printList(new Solution().minimumAbsDifference(new int[] { 4, 2, 1, 3 }));
+    }
+}
