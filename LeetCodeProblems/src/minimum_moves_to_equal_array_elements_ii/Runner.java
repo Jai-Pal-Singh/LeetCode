437479bf669f13299658fe88330b455407974130
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-12-21 11:18:42 +0530

    Minimum Moves to Equal Array Elements II

---
+++ LeetCodeProblems/src/minimum_moves_to_equal_array_elements_ii/Runner.java
@@ -0,0 +1,7 @@
+package minimum_moves_to_equal_array_elements_ii;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().minMoves2(new int[] {1,2,3}));
+    }
+}
