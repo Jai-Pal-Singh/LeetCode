358d6a64722abf1f7bbb262e7800cbb55109a2db
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-12-25 22:02:36 +0530

    Arithmetic Slices

---
+++ LeetCodeProblems/src/arithmetic_slices/Runner.java
@@ -0,0 +1,9 @@
+package arithmetic_slices;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().numberOfArithmeticSlices(new int[] { 1, 2, 3, 4 }));
+    }
+}
