4ae5babce232083d471e0b194eb3b08358310c5d
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-12-25 10:06:34 +0530

    Basic Calculator II

---
+++ LeetCodeProblems/src/basic_calculator_ii/Runner.java
@@ -0,0 +1,10 @@
+package basic_calculator_ii;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().calculate("3+2*2"));
+    }
+    
+}
