f543c1dc1888b8a22779546203165cbe350c63df
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-12-27 08:29:03 +0530

    Number Complement

---
+++ LeetCodeProblems/src/number_complement/Runner.java
@@ -0,0 +1,9 @@
+package number_complement;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().findComplement(5));
+    }
+}
