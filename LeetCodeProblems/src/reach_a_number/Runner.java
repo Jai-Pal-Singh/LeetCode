cd7ed612352e491370550073257df90f9377c031
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-12-28 12:09:40 +0530

    Reach a Number

---
+++ LeetCodeProblems/src/reach_a_number/Runner.java
@@ -0,0 +1,9 @@
+package reach_a_number;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().reachNumber(2));
+    }
+}
