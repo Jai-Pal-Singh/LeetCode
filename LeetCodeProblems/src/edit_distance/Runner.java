ded8c3806b8734f91262e53e796b519d2eb4808a
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-12-30 08:05:18 +0530

    Edit Distance

---
+++ LeetCodeProblems/src/edit_distance/Runner.java
@@ -0,0 +1,7 @@
+package edit_distance;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().minDistance("horse", "ros"));
+    }
+}
