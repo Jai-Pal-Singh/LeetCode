1b852baff22e09429c42481029ec581fa54be92a
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-12-31 12:03:29 +0530

    Combination Sum IV

---
+++ LeetCodeProblems/src/combination_sum_iv/Runner.java
@@ -0,0 +1,7 @@
+package combination_sum_iv;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().combinationSum4(new int[] { 1, 2, 3 }, 4));
+    }
+}
