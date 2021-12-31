ee92c087d9deb72eabbd18ac2631e91d2b94bc0d
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-12-31 12:44:11 +0530

    Trapping Rain Water

---
+++ LeetCodeProblems/src/trapping_rain_water/Runner.java
@@ -0,0 +1,7 @@
+package trapping_rain_water;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().trap(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }));
+    }
+}
