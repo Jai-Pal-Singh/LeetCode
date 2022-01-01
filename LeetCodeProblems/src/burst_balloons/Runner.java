2aa29d4d663f4ff464152841874d99f7179191ce
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-01 10:12:16 +0530

    Burst Balloons

---
+++ LeetCodeProblems/src/burst_balloons/Runner.java
@@ -0,0 +1,7 @@
+package burst_balloons;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().maxCoins(new int[] { 3, 1, 5, 8 }));
+    }
+}
