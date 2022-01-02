3b035d9e7016a67ea6b7f69a05d39ec1a27932f6
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-02 21:17:26 +0530

    Search in Rotated Sorted Array

---
+++ LeetCodeProblems/src/search_in_rotated_sorted_array/Runner.java
@@ -0,0 +1,7 @@
+package search_in_rotated_sorted_array;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().search(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 0));
+    }
+}
