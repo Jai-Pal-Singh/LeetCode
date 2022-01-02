e0e7aabf74344709af1b104b8b1f9d206a25b6ec
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-02 21:54:14 +0530

    Majority Element

---
+++ LeetCodeProblems/src/majority_element/Runner.java
@@ -0,0 +1,7 @@
+package majority_element;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().majorityElement(new int[] { 3, 2, 3 }));
+    }
+}
