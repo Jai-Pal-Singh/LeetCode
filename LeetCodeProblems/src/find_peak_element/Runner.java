e5e46be93daaabdd2cbc4f42f8d910e435125e04
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-02 12:40:51 +0530

    Find Peak Element

---
+++ LeetCodeProblems/src/find_peak_element/Runner.java
@@ -0,0 +1,7 @@
+package find_peak_element;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().findPeakElement(new int[] { 1, 2, 3, 1 }));
+    }
+}
