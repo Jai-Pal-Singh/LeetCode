190443a9281803b40fcfa97d25e48cc34c3cbf75
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-03 09:01:43 +0530

    Remove Duplicates from Sorted List II

---
+++ LeetCodeProblems/src/remove_duplicates_from_sorted_list_ii/Runner.java
@@ -0,0 +1,18 @@
+package remove_duplicates_from_sorted_list_ii;
+
+import helper.ListNode;
+
+public class Runner {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+        head.next.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next.next.next.next = new ListNode(5);
+        helper.ListNode.print(new Solution().deleteDuplicates(head));
+    }
+}
