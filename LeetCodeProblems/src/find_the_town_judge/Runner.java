928fdf597ea3e44d13a86a759b4c180416c0189d
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-03 08:13:47 +0530

    Find the Town Judge

---
+++ LeetCodeProblems/src/find_the_town_judge/Runner.java
@@ -0,0 +1,7 @@
+package find_the_town_judge;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().findJudge(2, new int[][] { { 1, 2 } }));
+    }
+}
