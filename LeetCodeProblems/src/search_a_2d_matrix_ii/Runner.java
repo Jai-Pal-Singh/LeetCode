6ca0e3aac5be7f078b224a6e33bbe2f90d374c7b
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-04 22:35:48 +0530

    Search a 2D Matrix II

---
+++ LeetCodeProblems/src/search_a_2d_matrix_ii/Runner.java
@@ -0,0 +1,7 @@
+package search_a_2d_matrix_ii;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().searchMatrix(new int[][]{{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}}, 5));
+    }
+}
