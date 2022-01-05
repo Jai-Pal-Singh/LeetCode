34201a8524c583cc95cc5fbfebf0be35c9ae4ec3
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-05 23:26:49 +0530

    Subarray Sum Equals K

---
+++ LeetCodeProblems/src/subarray_sum_equals_k/Runner.java
@@ -0,0 +1,7 @@
+package subarray_sum_equals_k;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().subarraySum(new int[]{1,1,1}, 2));
+    }
+}
