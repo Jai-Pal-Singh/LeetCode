2aaf0bef7f569090faf3592aabe0c4c8d080f3d8
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-05 21:53:56 +0530

    Minimum Size Subarray Sum

---
+++ LeetCodeProblems/src/minimum_size_subarray_sum/Runner.java
@@ -0,0 +1,7 @@
+package minimum_size_subarray_sum;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().minSubArrayLen(7, new int[]{2,3,1,2,4,3}));
+    }
+}
