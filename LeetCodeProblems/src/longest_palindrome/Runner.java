4840c13c90a4fad718d5c15bd45a8c90fe63eecf
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-06 22:01:39 +0530

    Longest Palindrome

---
+++ LeetCodeProblems/src/longest_palindrome/Runner.java
@@ -0,0 +1,7 @@
+package longest_palindrome;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().longestPalindrome("abccccdd"));
+    }
+}
