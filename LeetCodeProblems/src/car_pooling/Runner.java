3639a216bfd4686d6d5bdde62e37d0559d775ebc
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-06 20:11:38 +0530

    Car Pooling

---
+++ LeetCodeProblems/src/car_pooling/Runner.java
@@ -0,0 +1,7 @@
+package car_pooling;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().carPooling(new int[][]{{2,1,5},{3,3,7}}, 4));
+    }
+}
