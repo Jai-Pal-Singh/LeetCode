e7ccae5a3bf333974c5889a2d764da7181cd224a
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-06 21:45:18 +0530

    Number of Provinces

---
+++ LeetCodeProblems/src/number_of_provinces/Runner.java
@@ -0,0 +1,7 @@
+package number_of_provinces;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().findCircleNum(new int[][]{{1,1,0},{1,1,0},{0,0,1}}));
+    }
+}
---
+++ LeetCodeProblems/src/number_of_provinces/Solution.java
@@ -0,0 +1,27 @@
+package number_of_provinces;
+
+public class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int len = isConnected.length;
+        boolean[]visited = new boolean[len];
+        int count = 0;
+        
+        for(int i = 0; i < len ;i++){
+            if(!visited[i]){
+                count++;
+                dfs(isConnected,i,visited);
+            }
+        }
+      return count;  
+    }
+    
+    
+    private void dfs(int[][] isConnected,int i,boolean[]visited){
+        for(int j = 0 ; j < isConnected[i].length ; j++){
+            if(!visited[j] && isConnected[i][j] != 0){
+                visited[j] = true;
+                dfs(isConnected,j,visited);
+            }
+        }
+    }
+}
