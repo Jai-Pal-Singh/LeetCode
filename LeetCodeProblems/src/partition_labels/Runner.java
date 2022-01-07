2281f77724e6b573797dd0a2936e9ebb2eba78d9
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-07 09:55:08 +0530

    Partition Labels

---
+++ LeetCodeProblems/src/partition_labels/Runner.java
@@ -0,0 +1,7 @@
+package partition_labels;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.ArrayFunctions.print(new Solution().partitionLabels("ababcbacadefegdehijhklij"));
+    }
+}
