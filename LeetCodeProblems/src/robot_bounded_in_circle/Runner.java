ab10f55751ebb7c890496744bbd6cfffb99b5590
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-09 18:08:29 +0530

    Robot Bounded In Circle

---
+++ LeetCodeProblems/src/robot_bounded_in_circle/Runner.java
@@ -0,0 +1,7 @@
+package robot_bounded_in_circle;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().isRobotBounded("GL"));
+    }
+}
