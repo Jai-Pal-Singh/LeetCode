54b3cf54bd1020b86d3067931743c473e796012b
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-10 20:38:30 +0530

    Subsets

---
+++ LeetCodeProblems/src/subsets/Runner.java
@@ -0,0 +1,7 @@
+package subsets;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.ArrayFunctions.printList(new Solution().subsets(new int[] { 1, 2, 3 }));
+    }
+}
