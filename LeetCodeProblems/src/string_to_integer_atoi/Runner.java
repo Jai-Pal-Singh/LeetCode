c03995155a6e77bf63d4682e5d04ca642cb470b2
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-14 09:04:29 +0530

    String to Integer (atoi)

---
+++ LeetCodeProblems/src/string_to_integer_atoi/Runner.java
@@ -0,0 +1,7 @@
+package string_to_integer_atoi;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().myAtoi("-91283472332"));
+    }
+}
