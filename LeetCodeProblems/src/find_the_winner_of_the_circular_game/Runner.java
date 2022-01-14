9176d2f8c1808f1ea5c7102066ff47739715e716
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-14 21:26:44 +0530

    Find the Winner of the Circular Game

---
+++ LeetCodeProblems/src/find_the_winner_of_the_circular_game/Runner.java
@@ -0,0 +1,7 @@
+package find_the_winner_of_the_circular_game;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().findTheWinner(5, 2));
+    }
+}
