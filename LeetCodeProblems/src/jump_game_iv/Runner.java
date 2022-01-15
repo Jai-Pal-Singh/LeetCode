8c555b3783df2e464e87f7d836c06281508dbc5c
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-15 09:33:10 +0530

    Jump Game IV

---
+++ LeetCodeProblems/src/jump_game_iv/Runner.java
@@ -0,0 +1,7 @@
+package jump_game_iv;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().minJumps(new int[]{2, 3, 1, 1, 4}));
+    }
+}
---
+++ LeetCodeProblems/src/jump_game_iv/Solution.java
@@ -0,0 +1,123 @@
+package jump_game_iv;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+// Given an array of integers arr, you are initially positioned at the first index of the array.
+
+// In one step you can jump from index i to index:
+
+// i + 1 where: i + 1 < arr.length.
+// i - 1 where: i - 1 >= 0.
+// j where: arr[i] == arr[j] and i != j.
+// Return the minimum number of steps to reach the last index of the array.
+
+// Notice that you can not jump outside of the array at any time.
+
+ 
+
+// Example 1:
+
+// Input: arr = [100,-23,-23,404,100,23,23,23,3,404]
+// Output: 3
+// Explanation: You need three jumps from index 0 --> 4 --> 3 --> 9. Note that index 9 is the last index of the array.
+// Example 2:
+
+// Input: arr = [7]
+// Output: 0
+// Explanation: Start index is the last index. You do not need to jump.
+// Example 3:
+
+// Input: arr = [7,6,9,6,9,6,9,7]
+// Output: 1
+// Explanation: You can jump directly from index 0 to index 7 which is last index of the array.
+ 
+
+// Constraints:
+
+// 1 <= arr.length <= 5 * 104
+// -108 <= arr[i] <= 108
+
+// Solution
+// Overview
+// You probably can guess from the problem title, this is the fourth problem in the series of Jump Game problems. Those problems are similar, but have considerable differences, making their solutions quite different.
+
+// Here, two approaches are introduced: Breadth-First Search approach and Bidirectional BFS approach.
+
+// Approach 1: Breadth-First Search
+// Most solutions start from a brute force approach and are optimized by removing unnecessary calculations. Same as this one.
+
+// A naive brute force approach is to iterate all the possible routes and check if there is one reaches the last index. However, if we already checked one index, we do not need to check it again. We can mark the index as visited by storing them in a visited set.
+
+// From convenience, we can store nodes with the same value together in a graph dictionary. With this method, when searching, we do not need to iterate the whole list to find the nodes with the same value as the next steps, but only need to ask the precomputed dictionary. However, to prevent stepping back, we need to clear the dictionary after we get to that value.
+
+
+// Complexity Analysis
+
+// Assume NN is the length of arr.
+
+// Time complexity: \mathcal{O}(N)O(N) since we will visit every node at most once.
+
+// Space complexity: \mathcal{O}(N)O(N) since it needs curs and nex to store nodes.
+
+public class Solution {
+    public int minJumps(int[] arr) {
+        int n = arr.length;
+        if (n <= 1) {
+            return 0;
+        }
+
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            graph.computeIfAbsent(arr[i], v -> new LinkedList<>()).add(i);
+        }
+
+        List<Integer> curs = new LinkedList<>(); // store current layer
+        curs.add(0);
+        Set<Integer> visited = new HashSet<>();
+        int step = 0;
+
+        // when current layer exists
+        while (!curs.isEmpty()) {
+            List<Integer> nex = new LinkedList<>();
+
+            // iterate the layer
+            for (int node : curs) {
+                // check if reached end
+                if (node == n - 1) {
+                    return step;
+                }
+
+                // check same value
+                for (int child : graph.get(arr[node])) {
+                    if (!visited.contains(child)) {
+                        visited.add(child);
+                        nex.add(child);
+                    }
+                }
+
+                // clear the list to prevent redundant search
+                graph.get(arr[node]).clear();
+
+                // check neighbors
+                if (node + 1 < n && !visited.contains(node + 1)) {
+                    visited.add(node + 1);
+                    nex.add(node + 1);
+                }
+                if (node - 1 >= 0 && !visited.contains(node - 1)) {
+                    visited.add(node - 1);
+                    nex.add(node - 1);
+                }
+            }
+
+            curs = nex;
+            step++;
+        }
+
+        return -1;
+    }
+}
