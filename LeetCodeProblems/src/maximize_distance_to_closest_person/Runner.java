fd8dd0410f3c318c1179e97edc55b643ce75bdcf
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-17 08:19:56 +0530

    Maximize Distance to Closest Person

---
+++ LeetCodeProblems/src/maximize_distance_to_closest_person/Runner.java
@@ -0,0 +1,11 @@
+package maximize_distance_to_closest_person;
+
+/**
+ * Runner
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().maxDistToClosest(new int[] { 1, 0, 0, 0, 1, 0, 1 }));
+    }
+}
