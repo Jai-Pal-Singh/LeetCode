1ba8b68f0821dc69e711f7194431cf3a1f43dedc
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-18 08:06:35 +0530

    Can Place Flowers

---
+++ LeetCodeProblems/src/can_place_flowers/Runner.java
@@ -0,0 +1,7 @@
+package can_place_flowers;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.BasicFunctions.print(new Solution().canPlaceFlowers(new int[] { 1, 0, 0, 0, 1 }, 1));
+    }
+}
