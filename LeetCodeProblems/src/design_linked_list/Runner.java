ca093fef50754da623768d40fc61fa5bd127311a
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-19 16:55:37 +0530

    Design Linked List

---
+++ LeetCodeProblems/src/design_linked_list/Runner.java
@@ -0,0 +1,10 @@
+package design_linked_list;
+
+public class Runner {
+    public static void main(String[] args) {
+        MyLinkedList list = new MyLinkedList();
+        list.addAtHead(1);
+        list.addAtHead(2);
+        list.addAtHead(3);
+    }
+}
