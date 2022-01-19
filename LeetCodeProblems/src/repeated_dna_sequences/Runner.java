caff7a2d3e7aefd78ed4d5aee941e25cbb30a33b
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-19 16:26:22 +0530

    Repeated DNA Sequences

---
+++ LeetCodeProblems/src/repeated_dna_sequences/Runner.java
@@ -0,0 +1,9 @@
+package repeated_dna_sequences;
+
+import helper.ArrayFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        ArrayFunctions.printListString(new Solution().findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"));
+    }
+}
