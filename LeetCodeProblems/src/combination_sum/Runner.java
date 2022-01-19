828d3e948abe749954c3c551b2123f2bf0eab826
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-19 19:08:42 +0530

    Combination Sum

---
+++ LeetCodeProblems/src/combination_sum/Runner.java
@@ -0,0 +1,9 @@
+package combination_sum;
+
+import helper.ArrayFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        ArrayFunctions.printList(new Solution().combinationSum(new int[] {2,3,6,7}, 7));
+    }
+}
