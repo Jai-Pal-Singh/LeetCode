9a95005e1bbfa6eb79160c6c44b9d23b2bc43c69
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-19 18:36:07 +0530

    Generate Parentheses

---
+++ LeetCodeProblems/src/generate_parentheses/Runner.java
@@ -0,0 +1,7 @@
+package generate_parentheses;
+
+public class Runner {
+    public static void main(String[] args) {
+        helper.ArrayFunctions.printListString(new Solution().generateParenthesis(3));
+    }
+}
---
+++ LeetCodeProblems/src/generate_parentheses/Solution.java
@@ -0,0 +1,100 @@
+package generate_parentheses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+ 
+
+Constraints:
+
+1 <= n <= 8
+ */
+
+ /**
+  * Solution
+  Backtracking
+  Instead of adding '(' or ')' every time as in Approach 1, let's only add them when we know it will remain a valid sequence. We can do this by keeping track of the number of opening and closing brackets we have placed so far.
+
+We can start an opening bracket if we still have one (of n) left to place. And we can start a closing bracket if it would not exceed the number of opening brackets.
+
+
+Complexity Analysis
+
+Our complexity analysis rests on understanding how many elements there are in generateParenthesis(n). This analysis is outside the scope of this article, but it turns out this is the n-th Catalan number \dfrac{1}{n+1}\binom{2n}{n} 
+n+1
+1
+​
+ ( 
+n
+2n
+​
+ ), which is bounded asymptotically by \dfrac{4^n}{n\sqrt{n}} 
+n 
+n
+​
+ 
+4 
+n
+ 
+​
+ .
+
+Time Complexity : O(\dfrac{4^n}{\sqrt{n}})O( 
+n
+​
+ 
+4 
+n
+ 
+​
+ ). Each valid sequence has at most n steps during the backtracking procedure.
+
+Space Complexity : O(\dfrac{4^n}{\sqrt{n}})O( 
+n
+​
+ 
+4 
+n
+ 
+​
+ ), as described above, and using O(n)O(n) space to store the sequence.
+
+  */
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max){
+        if (cur.length() == max * 2) {
+            ans.add(cur.toString());
+            return;
+        }
+
+        if (open < max) {
+            cur.append("(");
+            backtrack(ans, cur, open+1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if (close < open) {
+            cur.append(")");
+            backtrack(ans, cur, open, close+1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+}
