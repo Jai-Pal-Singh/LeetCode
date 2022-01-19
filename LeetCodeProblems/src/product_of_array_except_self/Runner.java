b9609eaca612f62dfc230f1cfe01f05bfb60171d
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-19 13:45:06 +0530

    Product of Array Except Self

---
+++ LeetCodeProblems/src/product_of_array_except_self/Runner.java
@@ -0,0 +1,9 @@
+package product_of_array_except_self;
+
+import helper.ArrayFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        ArrayFunctions.print(new Solution().productExceptSelf(new int[] {1,2,3,4,5}));
+    }
+}
