84bb5fdb5295306e32c883e467239cf38ef30049
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-20 08:10:30 +0530

    Kth Largest Element in an Array

---
+++ LeetCodeProblems/src/kth_largest_element_in_an_array/Runner.java
@@ -0,0 +1,9 @@
+package kth_largest_element_in_an_array;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().findKthLargest(new int[] { 3, 2, 1, 5, 6, 4 }, 2));
+    }
+}
