6b8765f5038c1ceddf1b4cb4ba8556a90a45f1b8
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-21 19:49:55 +0530

    Sort Characters By Frequency

---
+++ LeetCodeProblems/src/sort_characters_by_frequency/Runner.java
@@ -0,0 +1,9 @@
+package sort_characters_by_frequency;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().frequencySort("tree"));
+    }
+}
