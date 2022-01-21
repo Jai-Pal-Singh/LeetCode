eda4382a57134025358ffad4de3c452b2039b5a3
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-21 20:03:27 +0530

    Happy Number

---
+++ LeetCodeProblems/src/happy_number/Runner.java
@@ -0,0 +1,9 @@
+package happy_number;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().isHappy(19));
+    }
+}
