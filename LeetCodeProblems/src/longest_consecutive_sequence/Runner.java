71e24d094cb708436812ce51c1b4e1cf7733c4d8
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-22 18:27:29 +0530

    Longest Consecutive Sequence

---
+++ LeetCodeProblems/src/longest_consecutive_sequence/Runner.java
@@ -0,0 +1,13 @@
+package longest_consecutive_sequence;
+
+import helper.BasicFunctions;
+
+/**
+ * Runner
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().longestConsecutive(new int[] { 100, 4, 200, 1, 3, 2 }));
+    }
+}
