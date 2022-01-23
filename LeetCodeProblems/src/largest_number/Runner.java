b78fbaab76494d939eb98fe5a1565a0b93ab1442
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-23 21:50:01 +0530

    Largest Number

---
+++ LeetCodeProblems/src/largest_number/Runner.java
@@ -0,0 +1,9 @@
+package largest_number;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().largestNumber(new int[] { 10, 2 }));
+    }
+}
