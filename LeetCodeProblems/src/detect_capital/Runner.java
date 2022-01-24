ee5b3abd2a7438e37ec7c341752e1d2cdb7146af
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-24 07:53:01 +0530

    Detect Capital

---
+++ LeetCodeProblems/src/detect_capital/Runner.java
@@ -0,0 +1,9 @@
+package detect_capital;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().detectCapitalUse("USA"));
+    }
+}
