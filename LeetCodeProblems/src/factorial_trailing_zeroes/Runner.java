109c02485473615f7e85eccb28caf4d65449bdd8
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-24 21:25:53 +0530

    Factorial Trailing Zeroes

---
+++ LeetCodeProblems/src/factorial_trailing_zeroes/Runner.java
@@ -0,0 +1,9 @@
+package factorial_trailing_zeroes;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().trailingZeroes(3));
+    }
+}
