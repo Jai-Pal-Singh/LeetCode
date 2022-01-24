3524684416fcafc81b198880628a96ed7027a8c5
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-24 08:22:12 +0530

    Odd Even Linked List

---
+++ LeetCodeProblems/src/odd_even_linked_list/Runner.java
@@ -0,0 +1,17 @@
+package odd_even_linked_list;
+
+import helper.ListNode;
+
+public class Runner {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        head.next.next.next.next.next.next = new ListNode(7);
+
+        ListNode.print(new Solution().oddEvenList(head));
+    }
+}
