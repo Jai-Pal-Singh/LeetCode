789e7f74fdd97f51150aa998821920b1340379dd
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-24 21:34:34 +0530

    Sum of Two Integers

---
+++ LeetCodeProblems/src/sum_of_two_integers/Runner.java
@@ -0,0 +1,9 @@
+package sum_of_two_integers;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().getSum(1, 2));
+    }
+}
