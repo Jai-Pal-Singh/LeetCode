c5666d1c2a701886002c51fcfe4301b7845dfd4c
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-27 18:15:25 +0530

    Find the Duplicate Number

---
+++ LeetCodeProblems/src/find_the_duplicate_number/Runner.java
@@ -0,0 +1,9 @@
+package find_the_duplicate_number;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().findDuplicate(new int[] { 1, 3, 4, 2, 2 }));
+    }
+}
