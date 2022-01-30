73bacc19727550f9ded965fb3b397443402e2d4a
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-30 16:15:45 +0530

    Excel Sheet Column Number

---
+++ LeetCodeProblems/src/excel_sheet_column_number/Runner.java
@@ -0,0 +1,9 @@
+package excel_sheet_column_number;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().titleToNumber("A"));
+    }
+}
