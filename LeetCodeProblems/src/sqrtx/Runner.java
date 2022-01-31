562a98446b775b0915ccf3ce2f2f4878d6e1d8e9
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-31 09:12:34 +0530

    Sqrt(x)

---
+++ LeetCodeProblems/src/sqrtx/Runner.java
@@ -0,0 +1,9 @@
+package sqrtx;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().mySqrt(8));
+    }
+}
---
+++ LeetCodeProblems/src/sqrtx/Solution.java
@@ -0,0 +1,49 @@
+package sqrtx;
+
+/**
+ * Given a non-negative integer x, compute and return the square root of x.
+
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+
+ 
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+ 
+
+Constraints:
+
+0 <= x <= 231 - 1
+ */
+
+ /**
+  * Look for the critical point: i * i <= x && (i+1)(i+1) > x
+A little trick is using i <= x / i for comparison, instead of i * i <= x, to avoid exceeding integer upper limit.
+
+Solution1 - Binary Search Solution: Time complexity = O(lg(x)) = O(32)=O(1)
+  */
+public class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+        int start = 1, end = x;
+        while (start < end) { 
+            int mid = start + (end - start) / 2;
+            if (mid <= x / mid && (mid + 1) > x / (mid + 1))// Found the result
+                return mid; 
+            else if (mid > x / mid)// Keep checking the left part
+                end = mid;
+            else
+                start = mid + 1;// Keep checking the right part
+        }
+        return start;
+    }
+}
