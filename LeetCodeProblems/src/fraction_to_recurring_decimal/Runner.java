b99b2b4960feb6a520d729a08841eeb3bbc63e2d
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-31 09:35:57 +0530

    Fraction to Recurring Decimal

---
+++ LeetCodeProblems/src/fraction_to_recurring_decimal/Runner.java
@@ -0,0 +1,9 @@
+package fraction_to_recurring_decimal;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().fractionToDecimal(1, 2));
+    }
+}
---
+++ LeetCodeProblems/src/fraction_to_recurring_decimal/Solution.java
@@ -0,0 +1,79 @@
+package fraction_to_recurring_decimal;
+
+import java.util.HashMap;
+
+/**
+ * Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
+
+If the fractional part is repeating, enclose the repeating part in parentheses.
+
+If multiple answers are possible, return any of them.
+
+It is guaranteed that the length of the answer string is less than 104 for all the given inputs.
+
+ 
+
+Example 1:
+
+Input: numerator = 1, denominator = 2
+Output: "0.5"
+Example 2:
+
+Input: numerator = 2, denominator = 1
+Output: "2"
+Example 3:
+
+Input: numerator = 4, denominator = 333
+Output: "0.(012)"
+ 
+
+Constraints:
+
+-231 <= numerator, denominator <= 231 - 1
+denominator != 0
+ */
+
+/**
+ * The important thing is to consider all edge cases while thinking this problem through, including: negative integer, possible overflow, etc.
+
+Use HashMap to store a remainder and its associated index while doing the division so that whenever a same remainder comes up, we know there is a repeating fractional part.
+ */
+public class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) {
+            return "0";
+        }
+        StringBuilder res = new StringBuilder();
+        // "+" or "-"
+        res.append(((numerator > 0) ^ (denominator > 0)) ? "-" : "");
+        long num = Math.abs((long)numerator);
+        long den = Math.abs((long)denominator);
+        
+        // integral part
+        res.append(num / den);
+        num %= den;
+        if (num == 0) {
+            return res.toString();
+        }
+        
+        // fractional part
+        res.append(".");
+        HashMap<Long, Integer> map = new HashMap<>();
+        map.put(num, res.length());
+        while (num != 0) {
+            num *= 10;
+            res.append(num / den);
+            num %= den;
+            if (map.containsKey(num)) {
+                int index = map.get(num);
+                res.insert(index, "(");
+                res.append(")");
+                break;
+            }
+            else {
+                map.put(num, res.length());
+            }
+        }
+        return res.toString();
+    }
+}
