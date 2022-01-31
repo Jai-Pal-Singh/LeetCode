edb38a7f2efb548e6fce53ec5cde32df586c6981
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-01-31 19:18:40 +0530

    Array Nesting

---
+++ LeetCodeProblems/src/array_nesting/Runner.java
@@ -0,0 +1,9 @@
+package array_nesting;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().arrayNesting(new int[]{5,4,0,3,1,6,2}));
+    }
+}
