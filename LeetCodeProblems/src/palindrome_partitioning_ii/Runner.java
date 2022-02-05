b0d35465186845a515ab2d51a735f7409972fce1
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-02-05 21:57:16 +0530

    Palindrome Partitioning II

---
+++ LeetCodeProblems/src/palindrome_partitioning_ii/Runner.java
@@ -0,0 +1,10 @@
+package palindrome_partitioning_ii;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        BasicFunctions.print(solution.minCut("aab"));
+    }
+}
