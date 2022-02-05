f220696020264b775aafa03aa784815efb5401d9
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-02-05 22:26:30 +0530

    Permutation Sequence

---
+++ LeetCodeProblems/src/permutation_sequence/Runner.java
@@ -0,0 +1,9 @@
+package permutation_sequence;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().getPermutation(3, 3));
+    }
+}
