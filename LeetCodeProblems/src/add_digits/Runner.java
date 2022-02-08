ace163345990fdbb3b4214861d4a139a2bb45772
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-02-08 07:43:45 +0530

    Add Digits

---
+++ LeetCodeProblems/src/add_digits/Runner.java
@@ -0,0 +1,10 @@
+package add_digits;
+
+import helper.BasicFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().addDigits(38));
+        BasicFunctions.print(new Solution2().addDigits(45));
+    }
+}
