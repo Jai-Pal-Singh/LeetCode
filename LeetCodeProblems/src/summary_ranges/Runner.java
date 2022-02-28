1a95cab158668cec7fb7f811699df6c13eaa2e0e
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-02-28 07:46:38 +0530

    Summary Ranges

---
+++ LeetCodeProblems/src/summary_ranges/Runner.java
@@ -0,0 +1,9 @@
+package summary_ranges;
+
+import helper.ArrayFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        ArrayFunctions.printListString(new Solution().summaryRanges(new int[]{0, 1, 2, 4, 5, 7}));
+    }
+}
