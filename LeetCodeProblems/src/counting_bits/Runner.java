2effaa526ff2be28fc6c1113168cf6c8bc6a62e0
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-03-01 09:55:17 +0530

    Counting Bits

---
+++ LeetCodeProblems/src/counting_bits/Runner.java
@@ -0,0 +1,9 @@
+package counting_bits;
+
+import helper.ArrayFunctions;
+
+public class Runner {
+    public static void main(String[] args) {
+        ArrayFunctions.print(new Solution().countBits(5));
+    }
+}
