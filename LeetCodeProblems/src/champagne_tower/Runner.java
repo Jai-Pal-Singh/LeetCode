8fc35eec8d6e558d428794ea681a5317a52953ec
Jai Pal Singh <jai.pal.2013@gmail.com>
2022-03-04 07:57:58 +0530

    Champagne Tower

---
+++ LeetCodeProblems/src/champagne_tower/Runner.java
@@ -0,0 +1,13 @@
+package champagne_tower;
+
+import helper.BasicFunctions;
+
+/**
+ * Runner
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+        BasicFunctions.print(new Solution().champagneTower(1, 1, 1));
+    }
+}
